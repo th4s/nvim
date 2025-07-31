1a81d117065c05b06e81bb67d8501b7d94be8b5a
th4s <th4s@metavoid.xyz>
2025-07-31 20:16:59 +0200

    add lean plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,11 +6,35 @@ return {
 	{
 		"ThePrimeagen/harpoon",
 		branch = "harpoon2",
-		dependencies = { "nvim-lua/plenary.nvim" }
+		dependencies = { "nvim-lua/plenary.nvim" },
 	},
 	{
-		'windwp/nvim-autopairs',
+		"windwp/nvim-autopairs",
 		event = "InsertEnter",
-		opts = {} -- this is equalent to setup({}) function
-	}
+		opts = {}, -- this is equalent to setup({}) function
+	},
+	{
+		"Julian/lean.nvim",
+		event = { "BufReadPre *.lean", "BufNewFile *.lean" },
+
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"nvim-lua/plenary.nvim",
+
+			-- optional dependencies:
+
+			-- a completion engine
+			--    hrsh7th/nvim-cmp or Saghen/blink.cmp are popular choices
+
+			-- 'nvim-telescope/telescope.nvim', -- for 2 Lean-specific pickers
+			-- 'andymass/vim-matchup',          -- for enhanced % motion behavior
+			-- 'andrewradev/switch.vim',        -- for switch support
+			-- 'tomtom/tcomment_vim',           -- for commenting
+		},
+
+		---@type lean.Config
+		opts = { -- see below for full configuration options
+			mappings = true,
+		},
+	},
 }
