d4be7196a11f98562188fa281ce3eb6ef0fb2781
th4s <th4s@metavoid.xyz>
2026-07-23 21:02:00 +0200

    add latex renderer and remove linter for markdown

---
+++ lua/custom/plugins/render-markdown.lua
@@ -0,0 +1,13 @@
+return {
+	"MeanderingProgrammer/render-markdown.nvim",
+	ft = { "markdown" },
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"echasnovski/mini.nvim",
+	},
+	opts = {
+		latex = {
+			enabled = true,
+		},
+	},
+}
