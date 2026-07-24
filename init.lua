381180321a208b89e5327015ca8c332b84258cb6
th4s <th4s@metavoid.xyz>
2026-07-24 20:31:58 +0200

    rollback conceal option

--- init.lua
+++ init.lua
@@ -98,9 +98,6 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
--- Enable conceallevel for markdown/latex rendering
-vim.opt.conceallevel = 2
-
 -- Make line numbers default
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
